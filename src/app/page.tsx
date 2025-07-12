7bc6bd09f3ba1b3898a271b139b5e1c298582027
Ayush Pandey <ayudgp9800@gmail.com>
2025-07-12 12:06:45 +0000

    some

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import { ArrowRight } from 'lucide-react';
 
 const categories = [
   { name: 'Half Sleeves', href: '/products?categories=Half+Sleeves', image: '/images/half.png', hint: 'oversized tshirt' },
-  { name: 'Full Sleeves', href: '/products?categories=Full+Sleeves', image: '/images/fullsleev.png', hint: 'long sleeve shirt' },
+  { name: 'Full Sleeves', href: '/products?categories=Full+Sleeves', image: '/images/full.png', hint: 'long sleeve shirt' },
   { name: 'Hoodies', href: '/products?categories=Hoodie', image: '/images/hoodies.png', hint: 'fashion hoodie' },
   { name: 'Sweatshirts', href: '/products?categories=Sweatshirt', image: '/images/sweatshirt.png', hint: 'sweatshirt model' },
 ];
